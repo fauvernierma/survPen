3d984a6c2400c12a54d61d3b302df13051a48d54
fauvernierma <49479976+fauvernierma@users.noreply.github.com>
2020-01-10 22:05:49 +0100

    Add files via upload

---
+++ src/rcpp_multmatvec.cpp
@@ -0,0 +1,32 @@
+
+#include <RcppEigen.h>
+
+// [[Rcpp::depends(RcppEigen)]]
+
+using Eigen::Map;                       // 'maps' rather than copies
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::VectorXd;                  // variable size vector, double precision
+
+//' Matrix multiplication between two matrices
+//'
+//' @param Mat1 a matrix.
+//' @param Mat2 another matrix.
+//' @return prod the product Mat1%*%Mat2
+//' @export
+// [[Rcpp::export(name="'%mult%'")]]
+MatrixXd multmat(const Map<MatrixXd> Mat1, const Map<MatrixXd> Mat2){
+ MatrixXd prod = Mat1*Mat2;
+ return(prod);
+}
+
+//' Matrix multiplication between a matrix and a vector
+//'
+//' @param Mat a matrix.
+//' @param vec a vector.
+//' @return prod the product Mat%*%vec
+//' @export
+// [[Rcpp::export(name="'%vec%'")]]
+VectorXd multvec(const Map<MatrixXd> Mat, const Map<VectorXd> vec){
+ VectorXd prod = Mat*vec;
+ return(prod);
+}
---
+++ src/survPen_types.h
@@ -0,0 +1,5 @@
+#include <RcppEigen.h>
+
+using Eigen::Map;                       // 'maps' rather than copies
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::VectorXd;                  // variable size vector, double precision
