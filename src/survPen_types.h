612c609b9a549d8175a83b6895e641422893f858
fauvernierma <49479976+fauvernierma@users.noreply.github.com>
2020-05-22 14:01:10 +0200

    Delete survPen_types.h

--- src/survPen_types.h
+++
@@ -1,5 +0,0 @@
-#include <RcppEigen.h>
-
-using Eigen::Map;                       // 'maps' rather than copies
-using Eigen::MatrixXd;                  // variable size matrix, double precision
-using Eigen::VectorXd;                  // variable size vector, double precision
